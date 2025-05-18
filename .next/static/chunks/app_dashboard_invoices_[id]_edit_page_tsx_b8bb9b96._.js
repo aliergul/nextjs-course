45f5855ba4ed61d00a7a6549072d7f7924a781de
ali <ali.ergul@pusula.net.tr>
2025-05-18 14:45:40 +0300

    chapter 13

--- .next/static/chunks/app_dashboard_invoices_[id]_edit_page_tsx_b8bb9b96._.js
+++ .next/static/chunks/app_dashboard_invoices_[id]_edit_page_tsx_b8bb9b96._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_eb6cac5f._.js"
+  "static/chunks/_2282633b._.js"
 ],
     source: "dynamic"
 });
